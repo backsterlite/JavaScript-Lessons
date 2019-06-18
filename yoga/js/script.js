169fdf044040da577e94549841d0ccdd3522cec1
backster <backstercorp@gmail.com>
2019-06-18 06:46:54 +0300

    create countdown timer

--- yoga/js/script.js
+++ yoga/js/script.js
@@ -33,6 +33,58 @@ window.addEventListener('DOMContentLoaded', function() {
             }
         }
      });
+
+     // Timer
+     let deadline = '2019-06-19';
+
+    function clockRemaining(endtime) {
+         let t       = Date.parse(endtime) - Date.parse(new Date()),
+             seconds = Math.floor((t/1000) % 60),
+             minutes = Math.floor((t/1000/60) % 60),
+             hours   = Math.floor((t/(1000*60*60)));
+
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+        
+        
+     }
+
+    function setClock(id, endtime) {
+         let t     = document.getElementById(id),
+             hours = document.querySelector('.hours'),
+             minutes = document.querySelector('.minutes'),
+             seconds = document.querySelector('.seconds'),
+             timeInterval = setInterval(updateClock, 1000);
+        
+        function updateClock() {
+            let t = clockRemaining(endtime);
+
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if(t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+
+            }
+            if(t.hours <=9) {
+                hours.textContent = '0' + t.hours;
+            }else if (t.minutes <= 9) {
+                minutes.textContent = '0' + t.minutes;
+            }else if(t.seconds <= 9) {
+                seconds.textContent = '0' + t.seconds;
+            }
+        }
+     }
+
+     setClock('timer', deadline);
         
 
 
